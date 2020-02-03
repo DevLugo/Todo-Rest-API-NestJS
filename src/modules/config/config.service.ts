2c5bcaa97a5f5deb2eb9d19c64f2a8cd29451264
Rodolfo Lugo <bl1ndcod3@gmail.com>
2020-02-03 01:02:10 -0500

    usamos puerto de server

--- src/modules/config/config.service.ts
+++ src/modules/config/config.service.ts
@@ -7,7 +7,7 @@ export class ConfigService {
     const isDevelopmentEnv = process.env.NODE_ENV !== 'production';
 
     this.envConfig = {
-      PORT: '9090',
+      PORT: process.env.PORT,
     };
   }
 

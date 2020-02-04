01e47e9fcabb5ab3a5ce169c1c4c8a8f54958b4a
Rodolfo Lugo <bl1ndcod3@gmail.com>
2020-02-03 21:45:27 -0500

    readme and live demo url

--- readme.mk
+++ readme.mk
@@ -1,5 +1,7 @@
 # TODO REST-API-JWT-Authentication with NestJS/Typescript 
-A Simple Rest API for a TODO-List with JWT-Authentication
+A Simple Rest API for a TODO-List with JWT-Authentication and deploy to azure with azure-pipelines
+### Live Demo: https://nestjs-todo-rest.azurewebsites.net/api/
+
 ### Tech
 We uses a number of open source projects to work properly:
 
@@ -7,44 +9,20 @@ We uses a number of open source projects to work properly:
 * [TypeScript] - TypeScript
 * [TypeORM] - ORM for Sql database
 * [PassportJs] - Authentication library
-* [dotenv] - python-dotenv
-* [docker] - docker
-* [docker-compose] - docker-compose
+* [NodeJs] - NodeJs V12.x
 
 ### Installation
 
-This boilerplate requires [Python](https://nodejs.org/) v3.6+ to run.
-
-Run the docker container wirh the following commands.
-
 ```sh
-$ cd Flask-Docker-SQLAlchemy-Tests
-$ docker-compose up -d
-$ docker exec -it {dockerid} bash
+$ cd npm install
+$ npm build
+$ npm run start
 ```
 
-And you need to modify the database conexion variables at ./app/.env file
-
-### Run (Inside on the docker container)
-
-```sh
-$ ./start.sh
-```
-
-### Migrations (Inside on the docker container)
-You need to create the db mannualy and then, write this on the terminal:
-
-```sh
-$ flask db init
-$ flask db migrate
-$ flask db upgrade
-```
-and done, this command create the full tables on the db
 
 ### Testing
-For testing purpose we use a diferente database.This databas will be empty because for every test the tables will'be create and delete.
 ```sh
-$ python tests.py
+$ npm run test
 ```
 
   [NestJs]: <https://nestjs.com/>
@@ -52,10 +30,6 @@ $ python tests.py
   [TypeORM]: <https://typeorm.io/>
   [PassportJs]: <http://www.passportjs.org//>
   [NodeJs]: <https://nodejs.org/es/>
-  [dotenv]: <https://www.npmjs.com/package/dotenv>
-  [docker]: <https://www.docker.com/>
-  [docker-compose]: <https://docs.docker.com/compose/>
-  
   
   By Rodolfo Lugo
   
